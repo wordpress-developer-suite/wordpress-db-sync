530233945f67cdfff47001a2c747e1fe681da679
Mike King <micjamking@users.noreply.github.com>
2015-09-26 13:53:33 -1000

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,10 @@
+# Comment out the following 2 lines if not using git to sync database.
+echo 'Getting latest updates from git...'; 
+git pull
+
+# Update lines 6-7 with your database name, user name, and password
+echo 'Updating the our MySQL database...';
+mysql --user='user' --password='password' db_name < ./db/data.sql
+mysql --user='user' --password='password' db_name < ./db/update.sql
+
+echo 'Update complete!';
