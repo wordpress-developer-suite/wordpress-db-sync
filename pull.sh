782260bd4f70d5d761866dff7b059342e1c8f569
Mike King <micjamking@users.noreply.github.com>
2015-11-05 17:12:26 -1000

    Updated comments

--- pull.sh
+++ pull.sh
@@ -2,7 +2,7 @@
 echo 'Getting latest updates from git...'; 
 git pull
 
-# Update lines 6-7 with your database name, user name, and password
+# Update lines 7-8 with your database name, user name, and password
 echo 'Updating our MySQL database...';
 mysql --user='user' --password='password' db_name < ./db/data.sql
 mysql --user='user' --password='password' db_name < ./db/update.sql
